2f6bdb2d2e6e9c0c0d9b1e862ddd359e1cf3f315
lambda <78629642+panbaoxiang@users.noreply.github.com>
2021-10-08 13:08:59 -0700

    Update Download_CFS.m

--- Download_CFS.m
+++ Download_CFS.m
@@ -1,4 +1,4 @@
-var={"prmsl", "q925", "q850", "q500", "z1000", "z850", "t2m","p"};
+var={"prmsl", "q925", "q850", "q500", "z1000", "z850", "tmp2m","prate"};
 day={"05","10","15","20","25","30"};
 hour={"00","06","12","18"};
 year=Map[ToString,Range[2011,2020]];
@@ -16,6 +16,8 @@ script=Flatten[Table[{"wget -nc https://www.ncei.noaa.gov/data/climate-forecast-
 
 Export["/Users/pan11/Documents/SADAN/Data/Forecast/forecast_realtime.sh",script,"Table"]
 
+
+
 years=Range[1982,2011];
 time=Flatten[Table[Block[{tempt=Flatten[Map[Table[DatePlus[#,{h,"Hour"}],{h,{0,6,12,18}}]&,Table[DatePlus[{year,1,1,0},day*5],{day,0,72}]],1]},
 	If[LeapYearQ[{year,1,1}],Map[If[#[[2]]>2,DatePlus[#,1],#]&,tempt],tempt]],{year,years}],1];
@@ -25,3 +27,8 @@ script=Flatten[Table[DateString[time[[t]],
 "Year","/","Year","Month","/",var[[k]]<>".","Year","Month","Day","Hour",".time.grb2"}],{t,Length[time]},{k,Length[var]-2}]];
 
 Export["/Users/pan11/Documents/SADAN/forecast_historical_prognostic.sh",script,"Table"]
+
+script=Flatten[Table[DateString[time[[t]],
+{"wget -nc https://www.ncei.noaa.gov/data/climate-forecast-system/access/reforecast/first-look/6-hourly-time-series-9-month/",
+var[[k]]<>"/","Year","Month","/",var[[k]]<>".","Year","Month","Day","Hour",".time.grb2"}],{t,Length[time]},{k,{-2,-1}}]];
+Export["/Users/pan11/Documents/SADAN/forecast_historical_diagnostic.sh",script,"Table"]
