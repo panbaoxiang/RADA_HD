03640e12d46b6a5480092e639c513d956761cc1d
lambda <78629642+panbaoxiang@users.noreply.github.com>
2021-11-20 09:39:17 -0800

    Update Download_CFS.m

--- Download_CFS.m
+++ Download_CFS.m
@@ -4,6 +4,7 @@ hour={"00","06","12","18"};
 year=Map[ToString,Range[2011,2020]];
 month={"01","02","03","04","05","06","07","08","09","10","11","12"};
 
+(*
 script=Flatten[Table[{"wget -nc https://www.ncei.noaa.gov/data/climate-forecast-system/access/reforecast/first-look/6-hourly-time-series-9-month/tmp2m/200407/tmp2m.200407"<>day[[i]]<>hour[[j]]<>".time.grb2",
 "wget -nc https://www.ncei.noaa.gov/data/climate-forecast-system/access/reforecast/first-look/6-hourly-time-series-9-month/prate/200407/prate.200407"<>day[[i]]<>hour[[j]]<>".time.grb2",
 "wget -nc https://www.ncei.noaa.gov/data/climate-forecast-system/access/reforecast/high-prioroty-subset/time-series-9-month/2004/200407/prmsl.200407"<>day[[i]]<>hour[[j]]<>".time.grb2",
@@ -15,7 +16,7 @@ script=Flatten[Table[{"wget -nc https://www.ncei.noaa.gov/data/climate-forecast-
 {i,Length[day]},{j,Length[hour]}]];
 
 Export["/Users/pan11/Documents/SADAN/Data/Forecast/forecast_realtime.sh",script,"Table"]
-
+*)
 
 
 years=Range[1982,2011];
